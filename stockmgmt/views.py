00e46f5fe82403fdfce48df6d51eae4e26411f92
Dhiraj Subedi <dhirajsubedi924@gmail.com>
2021-07-05 15:52:55 +0545

    render db data on templates

--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import*
 
 # Create your views here.
+
+
+def home(request):
+    title = "this is home page"
+    form = "this is home page"
+    context = {
+        'title': title,
+        'ok':form
+    }
+    return render(request, 'stockmgmt/home.html', context)
+
+def list_item(request):
+    
+    titile="this is title"
+    queryset = Stock.objects.all()
+    
+    context={
+            'queryset':queryset
+    }
+
+    return render(request,'stockmgmt/list_items.html', context)
