efa861a5b1651d7dd8d3168c3f4468012854b5e8
Dhiraj Subedi <dhirajsubedi924@gmail.com>
2021-07-06 14:27:25 +0545

    form + redirect +static

--- stockmgmt/urls.py
+++ stockmgmt/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('list_item', views.list_item, name="list_item")
+    path('list_item', views.list_item, name="list_item"),
+    path('add_item', views.add_item, name="add_item")
 
 ]
--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,25 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import*
+from .forms import *
 
 # Create your views here.
 
 
 def home(request):
     title = "this is home page"
-    form = "this is home page"
     context = {
         'title': title,
-        'ok':form
     }
     return render(request, 'stockmgmt/home.html', context)
+#  db ma vayeko data lai page ma dekhauna
+
 
 def list_item(request):
-    
-    titile="this is title"
+
+    titile = "this is title"
     queryset = Stock.objects.all()
-    
-    context={
-            'queryset':queryset
+
+    context = {
+        'queryset': queryset
     }
 
-    return render(request,'stockmgmt/list_items.html', context)
+    return render(request, 'stockmgmt/list_items.html', context)
+
+
+def add_item(request):
+    form = StockCreateForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('list_item')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'stockmgmt/add_item.html', context)
